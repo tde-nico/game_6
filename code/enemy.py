34b732a9fdc5fb43f56bdfc4e340c69dd0241dae
tde-nico <101901057+tde-nico@users.noreply.github.com>
2022-07-25 20:23:42 +0200

    earn exp

--- code/enemy.py
+++ code/enemy.py
@@ -1,4 +1,5 @@
 from turtle import distance
+from cv2 import add
 import pygame
 from settings import *
 from entity import Entity
@@ -6,7 +7,7 @@ from support import import_folder
 
 class Enemy(Entity):
 	def __init__(self, monster_name, pos, groups, obstacle_spites,
-		damage_player, trigger_death_particles):
+		damage_player, trigger_death_particles, add_exp):
 		super().__init__(groups)
 		self.sprite_type = 'enemy'
 		
@@ -34,6 +35,7 @@ class Enemy(Entity):
 		self.attack_cooldown = 400
 		self.damage_player = damage_player
 		self.trigger_death_particles = trigger_death_particles
+		self.add_exp = add_exp
 
 		self.vulnerable = True
 		self.hit_time = None
@@ -128,6 +130,7 @@ class Enemy(Entity):
 			self.kill()
 			self.trigger_death_particles(
 				self.rect.center, self.monster_name)
+			self.add_exp(self.exp)
 
 
 	def hit_reaction(self):
